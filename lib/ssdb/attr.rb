6004dd6d21dce4df5027b345291d4c92a730ea8a
Xuncheng Wang <w.xun.cheng@gmail.com>
2015-05-15 00:09:20 +0800

    covering more unit tests

--- lib/ssdb/attr.rb
+++ lib/ssdb/attr.rb
@@ -3,51 +3,54 @@ module SSDB
     extend ActiveSupport::Concern
 
     included do
-    #   puts 'called'
       define_model_callbacks :update_ssdb_attrs, only: [:before, :after]
+      after_destroy :clear_ssdb_attrs
     end
 
-    def to_ssdb_attr_key(name)
-      "#{self.class.to_s.underscore.pluralize}:#{self.id}:#{name}"
-    end
-
-    def update_ssdb_attrs(data={})
-
+    def update_ssdb_attrs(attributes)
       # Determine what attrs are requested to be updated
-      attr_names = data.keys & self.class.ssdb_attr_names
-      if attr_names.empty?
-        return
-      end
+      attr_names = attributes.keys & self.class.ssdb_attr_names
 
       # Determine dirty fields
       attr_names.each do |name|
-        if self.send(name.to_sym) != data[name].to_s
-          attribute_will_change! name.to_sym
-        end
+        send("#{name}_will_change!") unless attributes[name] == send(name)
       end
 
-      # trigger_ssdb_attr_before_callbacks if self.respond_to? :trigger_ssdb_attr_before_callbacks
       run_callbacks :update_ssdb_attrs do
         SSDBAttr.pool.with do |conn|
-          attr_names.each do |name|
-            conn.set("#{self.to_ssdb_attr_key(name)}", data[name])
-          end
+          attr_names.each { |name| conn.set("#{to_ssdb_attr_key(name)}", attributes[name]) }
         end
       end
 
-      # trigger_ssdb_attr_after_callbacks if self.respond_to? :trigger_ssdb_attr_after_callbacks
-
       # Clear dirty fields
-      attr_names.each do |name|
-        clear_attribute_changes name.to_sym
+      clear_attribute_changes(attr_names)
+    end
+
+    def clear_ssdb_attrs
+      self.class.ssdb_attr_names.each do |attribute|
+        SSDBAttr.pool.with { |conn| conn.del(to_ssdb_attr_key(attribute)) }
       end
+    end
+
+    def to_ssdb_attr_key(name)
+      self.class.to_ssdb_attr_key(name, id)
+    end
+
+    private
+
+    def touch_db_column(names)
+      names == true ? touch : touch(*names)
     end
 
     module ClassMethods
       def ssdb_attr_names
-        @ssdb_attr_names = [] if @ssdb_attr_names.nil?
-        @ssdb_attr_names
+        @ssdb_attr_names ||= []
       end
+
+      def to_ssdb_attr_key(name, id)
+        "#{self.name.tableize}:#{id}:#{name}"
+      end
+
 
       # ssdb_attr :content,        :string,   default: 0, touch: true
       # ssdb_attr :writer_version, :integer,  default: 0, touch: [:field1, :field2, :field3]
@@ -60,62 +63,31 @@ module SSDB
       #
       # @return [type] [description]
       def ssdb_attr(name, type, options={})
-
-        if ![:string, :integer].include?(type)
+        unless %i(string integer).include?(type)
           raise "Type not supported, only `:string` and `:integer` are supported now."
         end
 
-        self.ssdb_attr_names ||= []
         self.ssdb_attr_names << name
 
         define_method(name) do
           conversion = type == :string ? :to_s : :to_i
-
-          value = SSDBAttr.pool.with do |conn|
-            conn.get("#{self.to_ssdb_attr_key(name)}")
-          end
-
+          value = SSDBAttr.pool.with { |conn| conn.get("#{to_ssdb_attr_key(name)}") }
           value.try(conversion) || options[:default]
         end
 
-        define_method("#{name}=") do |v|
-          SSDBAttr.pool.with do |conn|
-            conn.set("#{self.to_ssdb_attr_key(name)}", v)
-          end
-
-          self.send("touch_ssdb_attr_#{name}".to_sym) if self.respond_to?("touch_ssdb_attr_#{name}".to_sym)
+        define_method("#{name}=") do |val|
+          SSDBAttr.pool.with { |conn| conn.set("#{to_ssdb_attr_key(name)}", val) }
+          touch_db_column(options[:touch]) if options[:touch].present?
         end
 
-        if options[:touch].present?
-          define_method("touch_ssdb_attr_#{name}") do
-            if options[:touch].kind_of?(Array)
-              touch(*options[:touch])
-            else
-              touch(:updated_at)
-            end
-          end
+        define_method("#{name}_will_change!") do
+          attribute_will_change!(name)
         end
 
         define_method("#{name}_changed?") do
-          attribute_changed?(name.to_sym)
+          attribute_changed?(name)
         end
       end
-
-      # def after_update_ssdb_attrs(*args)
-      #   define_method("trigger_ssdb_attr_after_callbacks") do
-      #     args.each do |arg|
-      #       self.send(arg.to_sym)
-      #     end
-      #   end
-      # end
-      #
-      # def before_update_ssdb_attrs(*args)
-      #   define_method("trigger_ssdb_attr_before_callbacks") do
-      #     args.each do |arg|
-      #       self.send(arg.to_sym)
-      #     end
-      #   end
-      # end
     end
   end
 end
---
+++ test/ssdb_attr_test.rb
@@ -0,0 +1,123 @@
+require 'ssdb-attr'
+require 'active_record'
+ActiveRecord::Base.raise_in_transactional_callbacks = true if ActiveRecord::VERSION::STRING >= '4.2'
+
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/pride'
+test_framework = defined?(MiniTest::Test) ? MiniTest::Test : MiniTest::Unit::TestCase
+
+require File.expand_path(File.dirname(__FILE__) + "/../lib/ssdb/attr")
+
+def connect!
+  SSDBAttr.setup(url: 'redis://localhost:6379/15')
+  ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+end
+
+def setup!
+  connect!
+  { 'posts' => 'updated_at DATETIME, saved_at DATETIME, changed_at DATETIME' }.each do |table_name, columns_as_sql_string|
+    ActiveRecord::Base.connection.execute "CREATE TABLE #{table_name} (id INTEGER NOT NULL PRIMARY KEY, #{columns_as_sql_string})"
+  end
+end
+
+setup!
+
+class Post < ActiveRecord::Base
+  include SSDB::Attr
+
+  ssdb_attr :name, :string
+  ssdb_attr :int_version, :integer
+  ssdb_attr :default_title, :string, default: "Untitled"
+  ssdb_attr :plain_touch, :string, touch: true
+  ssdb_attr :custom_touch_single_column, :string, touch: :saved_at
+  ssdb_attr :custom_touch_multiple_columns, :string, touch: [:saved_at, :changed_at]
+  ssdb_attr :title, :string
+  ssdb_attr :content, :string
+  ssdb_attr :version, :integer, default: 0
+
+  before_update_ssdb_attrs :before1, :before2
+  after_update_ssdb_attrs :after1, :after2
+
+  def callback_out
+    @callback_out ||= []
+  end
+
+  %i(before1 before2 after1 after2).each do |name|
+    define_method(name) do
+      callback_out << name
+    end
+  end
+end
+
+class SsdbAttrTest < test_framework
+  def setup
+    SSDBAttr.pool.with { |conn| conn.flushdb }
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.execute "DELETE FROM #{table}"
+    end
+    @post = Post.create(updated_at: 1.day.ago, saved_at: 1.day.ago, changed_at: 1.day.ago)
+  end
+
+  def test_respond_to_methods
+    assert_equal true, @post.respond_to?(:name)
+    assert_equal true, @post.respond_to?(:name=)
+    assert_equal true, @post.respond_to?(:name_changed?)
+    assert_equal true, @post.respond_to?(:name_will_change!)
+  end
+
+  def test_integer_attribute
+    @post.int_version = "4"
+    assert_equal 4, @post.int_version
+  end
+
+  def test_with_default_value
+    assert_equal "Untitled", @post.default_title
+  end
+
+  def test_with_plain_touch
+    original_updated_at = @post.updated_at
+    @post.plain_touch = "something"
+    refute_equal original_updated_at, @post.updated_at
+  end
+
+  def test_with_custom_touch_signle_column
+    original_saved_at = @post.saved_at
+    @post.custom_touch_single_column = "something"
+    refute_equal original_saved_at, @post.saved_at
+  end
+
+  def test_with_custom_touch_multiple_columns
+    original_saved_at, original_changed_at = @post.saved_at, @post.changed_at
+    @post.custom_touch_multiple_columns = "something"
+    refute_equal original_saved_at, @post.saved_at
+    refute_equal original_changed_at, @post.changed_at
+  end
+
+  def test_to_ssdb_attr_key
+    assert_equal "posts:#{@post.id}:name", Post.to_ssdb_attr_key("name", @post.id)
+  end
+
+  def test_update_ssdb_attrs
+    @post.update(title: "note one", content: "testing!!!", version: 1)
+    assert_equal "note one", @post.title
+    assert_equal "testing!!!", @post.content
+    assert_equal 1, @post.version
+  end
+
+  def test_update_ssdb_attrs_callbacks
+    @post.update_ssdb_attrs(title: "something")
+    assert_equal [:before1, :before2, :after1, :after2], @post.callback_out
+  end
+
+  def test_object_destroy_callbacks
+    @post.update_ssdb_attrs(title: "note one", content: "nice job!")
+    ssdb_title_key = @post.to_ssdb_attr_key(:title)
+    ssdb_content_key = @post.to_ssdb_attr_key(:content)
+    assert_equal true, SSDBAttr.pool.with { |conn| conn.exists(ssdb_title_key) }
+    assert_equal true, SSDBAttr.pool.with { |conn| conn.exists(ssdb_content_key) }
+    @post.destroy
+    assert_equal false, SSDBAttr.pool.with { |conn| conn.exists(ssdb_title_key) }
+    assert_equal false, SSDBAttr.pool.with { |conn| conn.exists(ssdb_content_key) }
+  end
+end
