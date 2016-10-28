ecb583882f21728d77643762b29d4e95e97f0b34
Larry Zhao <thehiddendepth@gmail.com>
2016-10-28 16:44:16 +0800

    switch to rspec

--- spec/ssdb/attr_spec.rb
+++ spec/ssdb/attr_spec.rb
@@ -44,55 +44,64 @@ describe SSDB::Attr do
   let(:post) { Post.create(updated_at: 1.day.ago, saved_at: 1.day.ago, changed_at: 1.day.ago) }
   let(:custom_id_field) { CustomIdField.create(uuid: 123) }
 
-  describe "respond to methods" do
-    it do
-      expect(post.respond_to?(:name)).to be_truthy
-      expect(post.respond_to?(:name=)).to be_truthy
-      expect(post.respond_to?(:name_was)).to be_truthy
-      expect(post.respond_to?(:name_change)).to be_truthy
-      expect(post.respond_to?(:name_changed?)).to be_truthy
-      expect(post.respond_to?(:restore_name!)).to be_truthy
-      expect(post.respond_to?(:name_will_change!)).to be_truthy
+  context "Post" do
+    describe "internal variables" do
+      it "should set `@ssdb_attr_names` correctly"  do
+        ssdb_attr_names = Post.instance_variable_get(:@ssdb_attr_names)
+        expect(ssdb_attr_names).to match_array(["name", "int_version", "default_title", "title", "content", "version"])
+      end
+    end
+
+    describe "respond to methods" do
+      it do
+        expect(post.respond_to?(:name)).to be_truthy
+        expect(post.respond_to?(:name=)).to be_truthy
+        expect(post.respond_to?(:name_was)).to be_truthy
+        expect(post.respond_to?(:name_change)).to be_truthy
+        expect(post.respond_to?(:name_changed?)).to be_truthy
+        expect(post.respond_to?(:restore_name!)).to be_truthy
+        expect(post.respond_to?(:name_will_change!)).to be_truthy
+      end
+    end
+
+    describe ".ssdb_attr_key" do
+      it "should return correct key" do
+        expect(post.send(:ssdb_attr_key, "name")).to eq("posts:#{post.id}:name")
+      end
+    end
+
+    # describe ".update_ssdb_attrs" do
+    #   context "with symbol keys" do
+    #     it "should update values correctly" do
+    #       post.update_ssdb_attrs(title: "note one", content: "testing!!!", version: 1)
+    #       post.reload_ssdb_attrs
+
+    #       expect(post.title).to eq("note one")
+    #       expect(post.content).to eq("testing!!!")
+    #       expect(post.version).to eq(1)
+    #     end
+    #   end
+    # end
+
+    context "type: :integer" do
+      it "default value should be 0" do
+        expect(post.int_version).to eq(0)
+      end
+
+      it "should hold integer value and return it" do
+        post.int_version = 4
+        expect(post.int_version).to eq(4)
+      end
+    end
+
+    context "type: :string" do
+      it "set default value should work" do
+        expect(post.default_title).to eq("Untitled")
+      end
     end
   end
 
-  describe ".ssdb_attr_key" do
-    it "should return correct key" do
-      expect(post.send(:ssdb_attr_key, "name")).to eq("posts:#{post.id}:name")
-    end
-  end
-
-  # describe ".update_ssdb_attrs" do
-  #   context "with symbol keys" do
-  #     it "should update values correctly" do
-  #       post.update_ssdb_attrs(title: "note one", content: "testing!!!", version: 1)
-  #       post.reload_ssdb_attrs
-
-  #       expect(post.title).to eq("note one")
-  #       expect(post.content).to eq("testing!!!")
-  #       expect(post.version).to eq(1)
-  #     end
-  #   end
-  # end
-
-  context "type: :integer" do
-    it "default value should be 0" do
-      expect(post.int_version).to eq(0)
-    end
-
-    it "should hold integer value and return it" do
-      post.int_version = 4
-      expect(post.int_version).to eq(4)
-    end
-  end
-
-  context "type: :string" do
-    it "set default value should work" do
-      expect(post.default_title).to eq("Untitled")
-    end
-  end
-
-  context "custom id field" do
+  context "CustomIdField" do
     it "should use the custom id correctly" do
       expect(CustomIdField.ssdb_attr_id_field).to eq(:uuid)
       expect(custom_id_field.send(:ssdb_attr_key, "content")).to eq("custom_id_fields:123:content")
